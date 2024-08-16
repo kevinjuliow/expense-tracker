88a5e3db1e7512ab9f0242d4c8fc15d6587ff747
Kevin Julio <kevjuliow@gmail.com>
2024-08-16 15:38:37 +0700

    dashboard created and expenses provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ApiProvider } from './api/ApiProvider';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApiProvider>
+
       <App />
-    </ApiProvider>
+  
   </React.StrictMode>
 );
 
